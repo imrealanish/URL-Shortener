cbae49199b6730a2839f65b5bb2b9270da585880
anish-ojas <anish@ojasenterprise.co.in>
2023-08-26 23:45:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgTinyUrlService } from 'ng-tiny-url';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'URLShortener';
+  urlText: any;
+  loading: boolean = false;
+  textCopied: boolean = false;
+  constructor(private tinyUrl: NgTinyUrlService) {
+  }
+  ngOnInit() {
+    const options = {
+      strings: ['Enter Your URL', 'And Get Shortener URL'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true
+    };
+    const typed = new Typed('.typed-element', options);
+  }
+  getUrlShorten(url: any) {
+    this.loading = true;
+    this.tinyUrl.shorten(url.value.url).subscribe((urlRes) => {
+      console.log(urlRes);
+      this.urlText = urlRes;
+      this.loading = false;
+    })
+  }
+  copyText() {
+    let inputEement = document.createElement('input');
+    inputEement.setAttribute('type', 'text');
+    inputEement.setAttribute('value', this.urlText);
+    inputEement.select();
+    inputEement.setSelectionRange(0, 9999);
+    try {
+      navigator.clipboard.writeText(inputEement.value);
+      this.textCopied = true;
+      setTimeout(() => {
+        this.textCopied = false;
+      }, 2000);
+    }
+    catch (err) {
+      console.log('Error whilte text copying....')
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgTinyUrlModule } from 'ng-tiny-url';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgTinyUrlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
